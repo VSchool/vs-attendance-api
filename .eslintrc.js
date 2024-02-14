fed70c92a14d0ad66c800c4611141834207eb6bf
Ben Turner <benturner@Bens-MacBook-Pro.local>
2024-02-14 03:11:56 +0000

    implement webpack

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,9 @@ module.exports = {
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  ignorePatterns: ["dist/**/*", "tmp/**/*"],
+  ignorePatterns: ["dist/**/*", "tmp/**/*", "**/*.js"],
+  env: {
+    node: true,
+  },
   rules: {},
 };

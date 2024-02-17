7aedd2ad3c85f4cb996399a996c6162397e727fa
Ben Turner <benturner@Bens-MacBook-Pro.local>
2024-02-17 20:18:50 +0000

    fix test

--- src/routers/qr-code.router.ts
+++ src/routers/qr-code.router.ts
@@ -8,7 +8,7 @@ const qrCodeRouter = ex.Router();
 qrCodeRouter.get("/generate", async (req, res) => {
   const accessToken = generateAccessToken();
   const payload = encodeURI(
-    `${process.env.ADMIN_CLIENT_BASE_URL}?access_token=${accessToken}`,
+    `${process.env.USER_CLIENT_BASE_URL}?access_token=${accessToken}`,
   );
   const dataUrl = await generateQRCode(payload);
   res.status(200).send({ dataUrl });

09eb95039d12070cdd13d06ad99412692479b5ef
Ben Turner <benturner@Bens-MacBook-Pro.local>
2024-02-20 03:44:44 +0000

    switch to refererr

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const server = ex();
 
 server.use(ex.json());
 server.use((req, res, next) => {
-  console.log(req.method, req.path, req.socket.remoteAddress);
+  console.log(req.method, req.path, req.headers.referer);
   next();
 });
 server.use(ex.static(path.resolve(__dirname, "..", "public")));

3f0717a05a36cc57880494459d0164a68d170f19
Ben Turner <benturner@Bens-MacBook-Pro.local>
2024-02-20 03:20:15 +0000

    update server logger

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const server = ex();
 
 server.use(ex.json());
 server.use((req, res, next) => {
-  console.log(req.method, req.hostname + req.path);
+  console.log(req.method, req.path, req.headers.host);
   next();
 });
 server.use(ex.static(path.resolve(__dirname, "..", "public")));
